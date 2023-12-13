a71e9d5ea78ae0781e68afa92634e64c7459782b
Viktoria <viktoria.polkina016@mail.ru>
2023-12-13 23:34:45 +0300

    PZ_6

--- PZ_5/pz_5.1.py
+++ PZ_5/pz_5.1.py
@@ -1,9 +1,13 @@
 # 24 вариант
 # составить функцию, которая выведет на экран строку, содержащую задаваемое с клавиатуры
 # число символов
-def print_string_with_length():
-    length = int(input("Введите число символов: "))
-    string = "*" * length  # создаем строку, содержащую нужное количество символов
-    print(string)
+def print_python_string(num_characters):
+    result = "^_^  " * num_characters
+    print(result)
 
-print_string_with_length()
+# Получаем ввод от пользователя
+num = int(input("Введите число символов: "))
+
+# Вызов функции для вывода строки
+print_python_string(num)
+
---
+++ PZ_6/PZ_6.1.py
@@ -0,0 +1,24 @@
+# вариант 24
+# Дан список ненулевых целых чисел размера N .
+# Проверить образуют ли его элементы геометрическую прогрессию.
+# Если образуют, то вывести знаменатель прогрессии, если нет - вывести 0
+def check_geometric_progression(arr):
+    if len(arr) < 2:
+        return 0
+
+    ratio = arr[1] / arr[0]
+
+    for i in range(1, len(arr)):
+        if arr[i] / arr[i - 1] != ratio:
+            return 0
+
+    return ratio
+
+
+# Пример использования:
+arr = [2, 4, 8, 16, 32]
+result = check_geometric_progression(arr)
+if result:
+    print(f"Элементы образуют геометрическую прогрессию с знаменателем {result}")
+else:
+    print("Элементы не образуют геометрическую прогрессию")
